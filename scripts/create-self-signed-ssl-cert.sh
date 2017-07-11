7d02f2c74453a32514c9bb02a6d79799e8ff9151
Thibault Maekelbergh <thibault.maekelbergh@icloud.com>
2017-07-11 17:43:02 +0200

    Add scripts to create openssl cert & letsencrypt cert

---
+++ scripts/create-self-signed-ssl-cert.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# -e <days>: generate a certificate with a certain expiriation in days (default: 9999)
+EXPIRY=9999
+
+while getopts "h?be:" opt; do
+  case $opt in
+    e) EXPIRY=$OPTARG
+      ;;
+    h|\?)
+      echo "Syntax: -e <days> to specify expiration for certificate" >&2
+      exit 1
+      ;;
+  esac
+done
+
+openssl req -x509 -newkey rsa:2048 -keyout key.pem -out cert.pem -days $EXPIRY
+openssl rsa -in key.pem -out key.pem
+openssl dhparam -out dhparams.pem 2048
---
+++ scripts/provisions/create-letsencrypt-cert.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+# @protected: location to download certbot-auto from
+_CERTBOT_URL=https://dl.eff.org/certbot-auto
+
+# -d <domain>: The FQDN to create a certificate for
+# -e <email>: Email address to sign the certificate to.
+DOMAIN=
+EMAIL=
+
+while getopts "h?d:e:" opt; do
+  case $opt in
+    d) DOMAIN=$OPTARG
+      ;;
+    e) EMAIL=$OPTARG
+      ;;
+    h|\?)
+      echo "Syntax: ./create-letsencrypt-cert.sh -d <domain> -e <email>" >&2
+      exit 1
+      ;;
+  esac
+done
+
+# Create a new isolated directory
+mkdir -p $HOME/.certbot
+wget -P $HOME/.certbot $_CERTBOT_URL
+chmod a+x $HOME/.certbot/certbot-auto
+
+# Generate a certificate at /etc/letsencrypt/live
+$HOME/.certbot/certbot-auto certonly --standalone \
+                                     --standalone-supported-challenges http-01 \
+                                     --email $EMAIL
+                                     -d $DOMAIN
+
+# Add auto-renewal script to the user's crontab
+(crontab -l 2>/dev/null; echo "40 11,23 * * * $HOME/.certbot/certbot-auto renew --quiet --no-self-upgrade >> $HOME/.certbot/certbot_renew.log") | crontab -
