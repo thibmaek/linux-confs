f660f2c7c6fcf418781ad4aa63925e789ef4f92a
Thibault Maekelbergh <thibault.maekelbergh@icloud.com>
2019-12-24 12:00:43 +0100

    telegraf: add inputs.mysql

--- etc/telegraf/telegraf_env.sample.sh
+++ etc/telegraf/telegraf_env.sample.sh
@@ -4,3 +4,8 @@ export INFLUXDB_HOST=""
 export INFLUXDB_DB="telegraf"
 export INFLUXDB_USER=""
 export INFLUXDB_PASSWORD=""
+
+export MYSQL_HOST=""
+export MYSQL_PORT="3306"
+export MYSQL_USER=""
+export MYSQL_PASSWORD=""
